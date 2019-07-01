25abc23b38808facca4859cd67acfd4dbe65fc3d
JulianBerger <jberger95@gmail.com>
2019-07-01 23:38:35 +0200

    removed unused code

--- backend/mqtt.js
+++ backend/mqtt.js
@@ -30,9 +30,7 @@ class MQTT {
     this.server.on('clientConnected', this.clientConnected);
     this.server.on('published', this.published);
     this.server.on('ready', this.afterSetup);
-
-    this.server.on('leaves', this.onLeavesReceive);
-    }
+  }
 
 
   clientConnected(client) {
@@ -44,20 +42,20 @@ class MQTT {
   published(packet, client) {
     console.log('Published', packet.topic, packet.payload);
 
-    if(packet.topic.startsWith('train')) {
+    if (packet.topic.startsWith('train')) {
       const trainDataSplit = packet.topic.substr(6).split('/');
       console.log(trainDataSplit);
-      if(trainDataSplit[0]) {
+      if (trainDataSplit[0]) {
         const trainIdInt = parseInt(trainDataSplit[0]);
         const route = trainDataSplit[1];
 
-        if(route === 'leaves') {
+        if (route === 'leaves') {
           self.onLeavesReceive(trainIdInt, packet, client);
-        } else if(route === 'speed') {
+        } else if (route === 'speed') {
           self.onSpeedReceive(trainIdInt, packet, client);
-        } else if(route === 'lat') {
+        } else if (route === 'lat') {
           self.onLatReceive(trainIdInt, packet, client);
-        } else if(route === 'lon') {
+        } else if (route === 'lon') {
           self.onLonReceive(trainIdInt, packet, client);
         }
 
@@ -74,25 +72,25 @@ class MQTT {
   onLeavesReceive(id, packet, client) {
     console.log('leeave:', id, packet);
     const leaves = parseInt(packet.payload) === 1;
-    TrainManager.updateTrain(id, {leaves: leaves});
+    TrainManager.updateTrain(id, { leaves: leaves });
   }
 
   onSpeedReceive(id, packet, client) {
     console.log('speed:', id, packet);
     const speed = parseFloat(packet.payload);
-    TrainManager.updateTrain(id, {speed: speed});
+    TrainManager.updateTrain(id, { speed: speed });
   }
 
   onLatReceive(id, packet, client) {
     console.log('lat:', id, packet);
     const lat = parseFloat(packet.payload);
-    TrainManager.updateTrain(id, {leavesLat: lat});
+    TrainManager.updateTrain(id, { leavesLat: lat });
   }
 
   onLonReceive(id, packet, client) {
     console.log('lon:', id, packet);
     const lon = parseFloat(packet.payload);
-    TrainManager.updateTrain(id, {leavesLon: lon});
+    TrainManager.updateTrain(id, { leavesLon: lon });
   }
 }
 
